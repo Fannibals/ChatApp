05db10c6b8d57bfb05256167cb506330391ca3d5
LCN1993 <snilove@hotmail.com>
2019-01-21 14:06:10 +0800

    set up

---
+++ Podfile
@@ -0,0 +1,8 @@
+target 'Smack-Demo' do
+
+pod 'Alamofire'
+pod 'SwiftyJSON'
+pod 'Socket.IO-Client-Swift'
+
+end
+
---
+++ Pods/Target Support Files/Pods-Smack-Demo/Pods-Smack-Demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Smack_Demo : NSObject
+@end
+@implementation PodsDummy_Pods_Smack_Demo
+@end
