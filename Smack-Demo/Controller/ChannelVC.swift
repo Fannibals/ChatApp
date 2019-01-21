4779d1672697bb3ff6c742a68fe58c4bd7d74f40
LCN1993 <snilove@hotmail.com>
2019-01-21 14:54:40 +0800

    updated: chatVC & channelVC

---
+++ Smack-Demo/Controller/ChannelVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChannelVC.swift
+//  Smack-Demo
+//
+//  Created by Ethan  on 21/1/19.
+//  Copyright © 2019 Ethan . All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
